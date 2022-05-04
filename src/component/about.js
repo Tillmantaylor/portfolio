9441bdac3f9b00257d0cbb3181eed1c9d25f0bdf
Tillmantaylor <Tillmant@etsu.edu>
2022-05-04 19:08:06 -0400

    favicon and title change

--- src/component/about.js
+++ src/component/about.js
@@ -12,7 +12,7 @@ const About = () => {
     <div id="about" className='container about-container' data-aos="fade-up-right" data-aos-duration="2000" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-center">
       <Home />
       <span className='about-header'>About Me</span> <br />
-      <span className='about-content'>I graduated from East Tennessee State University in May of 2022 with a bachelor's degree in computing and a concentration in Information Technology. I am most passionate about family, friends, and continuous learning. I currently reside in Elizabethton, Tennessee and will be relocating in July to Knoxville, Tennessee. I am a hard worker and a fast learner with a goal of finding a career in software engineering!</span>
+      <span className='about-content'>I am graduating Magna Cum Laude from East Tennessee State University in May of 2022 with a bachelor's degree in computing and a concentration in Information Technology. I am most passionate about family, friends, and continuous learning. I currently reside in Elizabethton, Tennessee and will be relocating in July to Knoxville, Tennessee. I am a hard worker and a fast learner with a goal of finding a career in software engineering!</span>
     </div>
   )
 }
